5651bbe5b1be69e3d7a5bec74afb31b7e3d6e856
lyoodong <dwryu3079@naver.com>
2023-09-21 14:53:27 +0900

    Initial Commit

---
+++ STTExample/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  STTExample
+//
+//  Created by Dongwan Ryoo on 2023/09/21.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    lazy var recordingSession: AVAudioSession = AVAudioSession.sharedInstance()
+    var audioRecorder: AVAudioRecorder!
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            
+            recordingSession.requestRecordPermission { permission in
+                if permission {
+                    print("음성 녹음 허용")
+                } else {
+                    print("음성 녹음 불허")
+                }
+            }
+            
+        } catch {
+            print("음성 녹음 실패")
+        }
+        
+        
+    }
+}
+
